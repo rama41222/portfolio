7bebd2b71a6dee4a2ae1ebe39255503c0c60ab1c
rama41222 <dinushankanrg@gmail.com>
2018-07-02 02:08:17 +0530

    hello text , menu boxes

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,82 @@
 <template>
   <div>
-    <h1 class="font-one">Rama</h1>
+    <b-container fluid class="center-home-dev">
+      <div class="intro">
+        <div class="typewriter">
+          <p class="font-one">HELLO</p>
+        </div>
+        <div class="">
+          <p class="font-two">I'M Dinushanka</p>
+        </div>
+        <div class="">
+          <o class="font-three">a FullStack Engineer</o>
+        </div>
+      </div>
+
+      <b-container class="menu">
+        <b-row>
+          <b-col sm="5">
+          </b-col>
+          <b-col>
+            <div class="menu-item">works</div>
+            <div class="menu-item">works</div>
+            <div class="menu-item">works</div>
+            <div class="menu-item">works</div>
+            <div class="menu-item">works</div>
+            <div class="menu-item">works</div>
+            <div class="menu-item">works</div>
+            <div class="menu-item">works</div>
+            <div class="menu-item">works</div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-container>
+
+
   </div>
 </template>
 
 <script>
 
   export default {
-    components: {
-
-    },
-    computed: {
-
-    },
+    components: {},
+    computed: {},
     mounted() {
 
     },
     data() {
-      return {
-
-      }
+      return {}
     },
-    methods: {
-
-    }
+    methods: {}
   }
 </script>
 
 <style type="scss">
+
+  .center-home-dev {
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+  }
+
+  .intro {
+    top: calc(50% - 6em);
+    left:10vw;
+    position: absolute;
+  }
+
+  .menu {
+    top: calc(30% - 6em);
+    right:10vw;
+    position: absolute;
+  }
+
+  .menu-item {
+    display: inline-block;
+    margin: 10px;
+    border: 2px solid;
+    padding: 50px;
+    box-shadow: 1px 2px 1px;
+  }
 
 </style>
